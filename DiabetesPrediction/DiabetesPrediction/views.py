62af4aff9252e6924253764622d0b1ca04daa781
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2021-02-20 14:40:06 +0530

    Add files

---
+++ DiabetesPrediction/DiabetesPrediction/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+import pandas as pd
+import numpy as np
+
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+
+def home(request):
+    return  render(request, 'home.html')
+
+def predict(request):
+    return render(request, 'predict.html')
+
+def result(request):
+    data = pd.read_csv('/home/vijay/Desktop/diabetes-prediction/data/diabetes.csv')
+
+    X = data.drop("Outcome",axis=1)
+    Y = data["Outcome"]
+    X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size = 0.2)
+
+    model = LogisticRegression()
+    model.fit(X_train,Y_train)
+
+    val1 = float(request.GET['n1'])
+    val2 = float(request.GET['n2'])
+    val3 = float(request.GET['n3'])
+    val4 = float(request.GET['n4'])
+    val5 = float(request.GET['n5'])
+    val6 = float(request.GET['n6'])
+    val7 = float(request.GET['n7'])
+    val8 = float(request.GET['n8'])
+    l = [val1,val2,val3,val4,val5,val6,val7,val8]
+    l = np.array(l)
+
+    pred = model.predict([l])
+    returnVal = None
+    if pred == [1]:
+        returnVal = "Positive"
+    else:
+        returnVal = "Negative"
+
+
+    return render(request, 'predict.html',{"result_prediction":returnVal})
